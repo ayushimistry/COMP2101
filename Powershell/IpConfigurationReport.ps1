3280e1056c0ae6031fc2fa61ca708d8943d2702c
ayushimistry <200465676@student.georgianc.on.ca>
2021-12-06 12:38:13 -0500

    LAB 3

--- Powershell/IpConfigurationReport.ps1
+++ Powershell/IpConfigurationReport.ps1
@@ -1,3 +1,2 @@
 $adapter = get-ciminstance win32_networkadapterconfiguration
-
 $adapter | where-object IPEnabled -eq 'true' | format-table Description,Index,IPAddress,IPSubnet,DNSDomain,DNSServerSearchOrder
