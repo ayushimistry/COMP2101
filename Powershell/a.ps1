321e1922f79cf64a8f631d729f4bb507d64c6cf9
ayushimistry <200465676@student.georgianc.on.ca>
2021-12-12 21:50:21 -0500

    practice

---
+++ Powershell/a.ps1
@@ -0,0 +1,5 @@
+﻿Param ([Parameter(Mandatory=$true)][string]$System,
+       [Parameter(Mandatory=$true)][string]$Disks,
+       [Parameter(Mandatory=$true)][string]$Network)
+
+if ($System -eq "System"){    get-cpuinfo    OsReport    MemoryReport    VideocardReport}else{    "Enter valid parameter"}
