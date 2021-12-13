749892f8a0ba241e0da4702bd0a0f8bd6bec9464
ayushimistry <90523285+ayushimistry@users.noreply.github.com>
2021-12-13 09:34:35 -0500

    Update SystemReport.ps1

--- Powershell/SystemReport.ps1
+++ Powershell/SystemReport.ps1
@@ -1,7 +1,33 @@
-﻿Param ([Parameter(Mandatory=$true)][string]$System,
+Param ([Parameter(Mandatory=$true)][string]$System,
        [Parameter(Mandatory=$true)][string]$Disks,
        [Parameter(Mandatory=$true)][string]$Network)
 
 
 Write-Output "-------------------------------------------------- "
-if ($System -eq "System"){get-cpuinfoOsReportMemoryReportVideocardReport}else{  "Enter valid parameter"}Write-Output "-------------------------------------------------- "if($Disks -eq "Disks"){DiskReport}else{"Enter valid parameter"}Write-Output "-------------------------------------------------- "if($Network -eq "Network"){NetworkReport}else{"Please write correct parameter"}break
+if ($System -eq "System"){
+get-cpuinfo
+OsReport
+MemoryReport
+VideocardReport
+}
+else
+{
+  "Enter valid parameter"
+}
+
+Write-Output "-------------------------------------------------- "
+if($Disks -eq "Disks"){
+DiskReport
+}
+else{
+"Enter valid parameter"
+}
+
+Write-Output "-------------------------------------------------- "
+if($Network -eq "Network"){
+NetworkReport
+}
+else{
+"Please write correct parameter"
+}
+break
