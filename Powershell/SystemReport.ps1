0af526669e2c44b99a2923efb11c4d889d5e04ec
ayushimistry <200465676@student.georgianc.on.ca>
2021-12-13 10:17:48 -0500

    LAB 5

--- Powershell/SystemReport.ps1
+++ Powershell/SystemReport.ps1
@@ -1,33 +1,7 @@
-Param ([Parameter(Mandatory=$true)][string]$System,
-       [Parameter(Mandatory=$true)][string]$Disks,
-       [Parameter(Mandatory=$true)][string]$Network)
+﻿Param ([Parameter][string]$System,
+       [Parameter][string]$Disks,
+       [Parameter][string]$Network)
 
 
 Write-Output "-------------------------------------------------- "
-if ($System -eq "System"){
-get-cpuinfo
-OsReport
-MemoryReport
-VideocardReport
-}
-else
-{
-  "Enter valid parameter"
-}
-
-Write-Output "-------------------------------------------------- "
-if($Disks -eq "Disks"){
-DiskReport
-}
-else{
-"Enter valid parameter"
-}
-
-Write-Output "-------------------------------------------------- "
-if($Network -eq "Network"){
-NetworkReport
-}
-else{
-"Please write correct parameter"
-}
-break
+if ($System -eq 'true' -Or $System -ne 'true'){get-cpuinfoOsReportMemoryReportVideocardReport}Write-Output "-------------------------------------------------- "if($Disks -eq 'true' -Or $Disks -ne 'true'){DiskReport}Write-Output "-------------------------------------------------- "if($Network -eq 'true'-Or $Network -ne 'true'){NetworkReport}break
